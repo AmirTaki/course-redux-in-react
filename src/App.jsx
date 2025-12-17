9d473db223f003881eb793d94dc09f990323f4de
amir <amirtaki623@gmail.com>
2025-12-17 18:45:26 +0330

    create react js & install tailwin in projec

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-rose-400">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat recusandae asperiores cumque, alias reiciendis officiis provident eveniet magnam nobis odio illum reprehenderit earum quaerat eligendi, repellat repellendus ipsam rem corporis!
+      </div>
+    </>
+  )
+}
+
+export default App
