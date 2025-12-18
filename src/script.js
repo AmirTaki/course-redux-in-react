adf60908aaefbee18cc5a7cb00cd19e6fcce1957
amir <amirtaki623@gmail.com>
2025-12-19 03:04:53 +0330

    جریان داده در ریداکس

--- src/script.js
+++ src/script.js
@@ -1,29 +1,32 @@
+// store
 const store = {
     state : {
         cart: [], show: []
     },
 
-    // view
+   
     getState (){
         return this.state
     },
 
-    // update
+    // method dispatch
     dispatch(action) {
       this.state =  reducer(this.state, action)
     }
 }
 
-
+// reducer
 function reducer (state, action) {
     switch(action.type){
         case "ADD-TO-CART":
+            // state new 
             return {...state, cart : [
                 ...state.cart,
                 action.payload
             ]}
         
         case "REMOVE-FROM-CART":
+            // state new
             return {...state, cart: [
                 ...state.cart.filter((item) => item !== action.payload )
             ]}
@@ -41,6 +44,7 @@ function removeFromCart(product){
     return {type: "REMOVE-FROM-CART", payload: product}
 }
 
+// action 
 store.dispatch(addToCart('lenove'))
 store.dispatch(addToCart('asus'))
 store.dispatch(removeFromCart('lenove'))
