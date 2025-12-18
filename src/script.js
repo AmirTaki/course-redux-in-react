f2ad5e510ae9adfd2bd506353c6a025976ab7bd5
amir <amirtaki623@gmail.com>
2025-12-19 02:50:07 +0330

    Reducer

--- src/script.js
+++ src/script.js
@@ -1,50 +1,51 @@
-// const store = {
-//     state : {
-//         cart: [], show: []
-//     },
+const store = {
+    state : {
+        cart: [], show: []
+    },
 
-//     // view
-//     getState (){
-//         return this.state
-//     },
+    // view
+    getState (){
+        return this.state
+    },
 
-//     // update
-//     dispatch(action) {
-//         if(action.type === "ADD-TO-CART"){
-//             this.state.cart.push(action.payload)       
-//         }
-//         else if (action.type === 'REMOVE-FROM-CART'){
-//             this.state.cart.pop()
-//         }
-//     }
-// }
-
-// function addToCart(product) {
-//     return {type: "ADD-TO-CART", payload: product}
-// }
-
-// function removeFromCart(product){
-//     return {type: "REMOVE-FROM-CART", payload: product}
-// }
-
-// store.dispatch(addToCart('lenove'))
-// store.dispatch(removeFromCart('lenove'))
-
-// console.log(store.getState())
+    // update
+    dispatch(action) {
+      this.state =  reducer(this.state, action)
+    }
+}
 
 
+function reducer (state, action) {
+    switch(action.type){
+        case "ADD-TO-CART":
+            return {...state, cart : [
+                ...state.cart,
+                action.payload
+            ]}
+        
+        case "REMOVE-FROM-CART":
+            return {...state, cart: [
+                ...state.cart.filter((item) => item !== action.payload )
+            ]}
+        
+        default: 
+            return {state}
+    }
+}
 
-let name = ['amir', 'mani', 'shayan']
-// let copyName = name
-let copyName = [...name]
-copyName[2] = 'javad'
-console.log("name: ",name)
-console.log("CopyName: ",copyName)
+function addToCart(product) {
+    return {type: "ADD-TO-CART", payload: product}
+}
+
+function removeFromCart(product){
+    return {type: "REMOVE-FROM-CART", payload: product}
+}
+
+store.dispatch(addToCart('lenove'))
+store.dispatch(addToCart('asus'))
+store.dispatch(removeFromCart('lenove'))
+
+console.log(store.getState())
 
 
-let obj = {name: 'amir', job: 'fornt end', city: 'karaj'}
-// let copyOjb = obj
-let copyOjb = {...obj,name : 'shayan'}
-copyOjb.job = 'back end'
-console.log('obj: ', obj)
-console.log('copyObj: ', copyOjb)
+
