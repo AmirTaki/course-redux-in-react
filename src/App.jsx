434bc1210c066de7df2d48a5f8e83054bf5e9ff7
amir <amirtaki623@gmail.com>
2025-12-19 05:37:19 +0330

    نصب و راه اندازی ریداکس part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrease, increase } from './REDUX/actions'
+
 
 function App() {
 
+  let counter =  useSelector((state) => state.counter)
+  console.log(counter)
+
+  let  dispatch =  useDispatch()
+  console.log(dispatch)
+
   return (
-    <>
-      <div className="text-rose-400">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat recusandae asperiores cumque, alias reiciendis officiis provident eveniet magnam nobis odio illum reprehenderit earum quaerat eligendi, repellat repellendus ipsam rem corporis!
-      </div>
-    </>
+    <div className="text-rose-400 ">
+
+      <h1>Counter value is : {counter} </h1>
+
+      <button onClick={() => {dispatch(increase())}}>increase</button>
+
+      <button onClick={() => {dispatch(decrease())}}>decrease</button>
+
+    </div>
   )
 }
 
