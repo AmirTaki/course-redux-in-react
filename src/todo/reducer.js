e02e501b0bd0bf8ba4fc37d026e3e128358e6912
amir <amirtaki623@gmail.com>
2025-12-20 05:10:01 +0330

    Local Storage in project ToDoList

--- src/todo/reducer.js
+++ src/todo/reducer.js
@@ -1,26 +1,23 @@
 import {v4 as uuidv4 } from "uuid";
 
 const initilState = {
-    todos : [],
+    todos :  JSON.parse(localStorage.getItem('todos')) || []
 }
 
 
 const toDoReducer = (state = initilState, action) => {
     switch(action.type){
-
         case "ADD":
-            return {...state, todos: [
-                ...state.todos, 
-                {
-                    text: action.payload.text ,
-                    id : uuidv4()
-                }
-            ]}
+            let addToDos = [...state.todos, {text: action.payload.text, id: uuidv4()}]
+            localStorage.setItem('todos', JSON.stringify(addToDos))
+            return {...state, todos: addToDos}
+
 
         case "REMOVE": 
-            return {...state, 
-                todos: [...state.todos.filter((item) => item.id !== action.payload.id )]
-            }
+            let updatedTodos = [...state.todos.filter((item) => item.id !== action.payload.id)]
+            localStorage.setItem("todos", JSON.stringify(updatedTodos))
+            return {...state, todos: updatedTodos}    
+        
         default : 
             return state
     }
